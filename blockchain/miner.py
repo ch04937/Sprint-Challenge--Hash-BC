6ac589bd736d81227450580e5c53e5a1fea089da
ch04937 <97hernandez.c@gmail.com>
2020-02-14 11:53:28 -0800

    waiting for miner

--- blockchain/miner.py
+++ blockchain/miner.py
@@ -23,8 +23,13 @@ def proof_of_work(last_proof):
     start = timer()
 
     print("Searching for next proof")
-    proof = 0
-    #  TODO: Your code here
+    proof = random.random()
+    #  TODO: Your code
+    end_proof = f'{last_proof}'.encode()
+    t_hash = hashlib.sha256(end_proof).hexdigest()
+
+    while valid_proof(t_hash, proof) is False:
+        proof
 
     print("Proof found: " + str(proof) + " in " + str(timer() - start))
     return proof
@@ -38,9 +43,11 @@ def valid_proof(last_hash, proof):
 
     IE:  last_hash: ...AE9123456, new hash 123456E88...
     """
-
     # TODO: Your code here!
-    pass
+    guess = f'{proof}'.encode()
+    l_hash = hashlib.sha256(guess).hexdigest()
+
+    return l_hash[:6] == last_hash[:6]
 
 
 if __name__ == '__main__':
@@ -48,7 +55,7 @@ if __name__ == '__main__':
     if len(sys.argv) > 1:
         node = sys.argv[1]
     else:
-        node = "https://lambda-coin.herokuapp.com/api"
+        node = "https://lambda-coin-test-1.herokuapp.com/api"
 
     coins_mined = 0
 
--- hashtables/ex2/ex2.py
+++ hashtables/ex2/ex2.py
@@ -14,10 +14,34 @@ class Ticket:
 
 def reconstruct_trip(tickets, length):
     hashtable = HashTable(length)
-    route = [None] * length
+    route = [None] * (length-1)
 
     """
     YOUR CODE HERE
     """
 
-    pass
+    # for each ticket in tickets insert all to to hashtable
+    for ticket in tickets:
+        # with key = source and value = destination
+
+        hash_table_insert(hashtable, ticket.source, ticket.destination)
+
+    current_source = "NONE"
+    # retrieve routes -1
+    for i in range(length-1):
+        # set routes to retrive and add them to the route
+        route[i] = hash_table_retrieve(hashtable, current_source)
+        current_source = route[i]
+
+    return route
+
+
+ticket_1 = Ticket("NONE", "PDX")
+ticket_2 = Ticket("PDX", "DCA")
+ticket_3 = Ticket("DCA", "NONE")
+
+tickets = [ticket_1, ticket_2, ticket_3]
+
+results = reconstruct_trip(tickets, 3)
+
+print(f'\n resulst: ', results)
